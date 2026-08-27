0937fcd5a881c6aab7ae191da9234d15049166e1
Arjun <arjunprajeev10@gmail.com>
2026-08-27 10:14:01 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,11 @@
+print("Welcome to git")
+
+'''
+
+git config --global user.name "Arjun"
+git config --global user.email "arjunprajeev10@gmail.com"
+
+git init - initialising git, after this git will track
+
+git status - prints the tracked files
+'''
