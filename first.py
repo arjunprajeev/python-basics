fb3216e29a154ef8973b2882f84f4768f3eff6d4
Arjun <arjunprajeev10@gmail.com>
2026-08-27 10:22:35 +0530

    modified git contents

--- first.py
+++ first.py
@@ -1,11 +1,13 @@
 print("Welcome to git")
 
 '''
-
+git - centralized folder in the system
 git config --global user.name "Arjun"
 git config --global user.email "arjunprajeev10@gmail.com"
 
 git init - initialising git, after this git will track
 
 git status - prints the tracked files
+git add <fileame>- converts to staged changes, it confims the changes made
+git add . - all files
 '''
