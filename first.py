a744c8cabb160a6debcb6baebc7025ae41dbc600
Arjun <arjunprajeev10@gmail.com>
2026-08-28 09:55:00 +0530

    added remote command

--- first.py
+++ first.py
@@ -10,4 +10,15 @@ git init - initialising git, after this git will track
 git status - prints the tracked files
 git add <fileame>- converts to staged changes, it confims the changes made
 git add . - all files
+
+connecting git and github
+    git remote add origin https://github.com/arjunprajeev/python-basics.git
+
+git and github should use same branch nae, then only pushing is possible
+git master is the branch, github main is the branch
+git branch main - create new branch
+git branch - to list the available branches
+git checkout branch_name - to change the branch 
+
+git remote -v - to check which account is connected
 '''
